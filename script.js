fe1dbed68f90cf88a3967ea7d7ce2663f0f7db94
nikothegamer <95710125+nikothegamer100@users.noreply.github.com>
2022-09-22 19:04:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,34 @@ const letters = {
     "√":"z"
     
 }
+const letters1 = {
+    "a": "σ",
+    "b": "£",
+    "c": "Ǝ",
+    "d": "₳",
+    "e": "ε",
+    "f": "┘",
+    "g":  "Γ",
+   "h": "μ",
+   "i": "∩",
+   "j": "ſ",
+   "k": "≡",
+    "l":"Œ",
+   "m":"ß",
+   "n": "þ",
+   "o":"⌐",
+   "p": "Æ",
+    "q":"¶",
+    "r": "Ω",
+    "s":"Φ",
+    "t": "┼",
+    "u":"↨",
+    "v": "‡",
+    "w": "w",
+    "x":"idk",
+    "z":"√"
+    
+}
 function translate() {
     const text = document.getElementById("text").value
 
@@ -35,7 +63,7 @@ function translate() {
     for (var i = 0; i < text.length; i++) {
         translated + letters[text.charAt(i)]
         if(letters[text.charAt(i)] == undefined) {
-            document.getElementById("results").innerText = document.getElementById("results").innerText + ""
+            document.getElementById("results").innerText = document.getElementById("results").innerText + " "
             
         } else {
             document.getElementById("results").innerText = document.getElementById("results").innerText + letters[text.charAt(i)]
@@ -43,4 +71,19 @@ function translate() {
         console.log(letters[text.charAt(i)])
       }
       
+}
+
+function texttolatexlanguage() {
+    const text = document.getElementById("anotherthing").value
+    document.getElementById("results1").innerText = ""
+    for (var i = 0; i < text.length; i++) {
+      
+        if(letters1[text.charAt(i)] == undefined) {
+            document.getElementById("results1").innerText = document.getElementById("results").innerText + " "
+            
+        } else {
+            document.getElementById("results1").innerText = document.getElementById("results1").innerText + letters1[text.charAt(i)]
+        }
+        console.log(letters1[text.charAt(i)])
+      }
 }
